e4af452058db253dc947932e0e000e93372c0009
deepashrangan7 <6971142-Deepashrangan@users.noreply.gitlab.com>
2020-12-01 17:54:41 +0530

    final code

--- src/main/java/com/cts/VotingSystemAuthServiceApplication.java
+++ src/main/java/com/cts/VotingSystemAuthServiceApplication.java
@@ -3,7 +3,9 @@ package com.cts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
+import brave.sampler.Sampler;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -14,6 +16,11 @@ public class VotingSystemAuthServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VotingSystemAuthServiceApplication.class, args);
 		log.info("server started at 8100");
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
